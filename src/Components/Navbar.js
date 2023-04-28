090f51ac1ea73eff8f66e24c2fa23880f25147a9
Kamal Soni <iamkamalsoni7@gmail.com>
2023-04-28 20:20:47 +1000

    fullly responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,6 +5,7 @@ import { BiMenu } from 'react-icons/bi'
 import { RxCrossCircled } from 'react-icons/rx'
 import SingleNavLink from './SingleNavLink'
 import { MdDarkMode, MdOutlineDarkMode } from 'react-icons/md'
+import { FiSun } from 'react-icons/fi'
 
 import Search from './Search'
 
@@ -41,20 +42,20 @@ const Navbar = () => {
 
   return (
     <div
-      className={`h-28 sm:h-24 ${
+      className={`   px-2 md:px-8 py-4 md:py-6 ${
         isDark
           ? 'bg-gradient-to-r from-zinc-800 via-slate-800 to-stone-800'
           : 'bg-gradient-to-r from-blue-800 via-blue-600 to-blue-900'
-      } flex items-center justify-between flex-wrap z-50`}
+      } flex items-center justify-between z-50 `}
     >
       <img
-        className="rounded-xl h-11 ml-4 sm:ml-5 md:ml-10 "
+        className=" w-[15%] md:w-[8%] rounded-xl h-11 block"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/1200px-IMDB_Logo_2016.svg.png"
         alt=""
         onClick={() => navigate('/')}
       />
 
-      <ul className=" hidden lg:flex lg:gap-8 xl:gap-10 font-normal text-xl text-white ">
+      <ul className=" w-[60%] hidden lg:flex items-center justify-center lg:gap-8 xl:gap-10 font-normal text-xl text-white ">
         <SingleNavLink to="/" linkName="Home" />
         <SingleNavLink to="/explore_movies" linkName="Explore Movies" />
         <SingleNavLink to="/contact_us" linkName="Contact Us" />
@@ -67,7 +68,7 @@ const Navbar = () => {
             ? 'bg-gradient-to-r from-zinc-800 via-slate-800 to-stone-800'
             : 'bg-gradient-to-r from-blue-800 via-blue-600 to-blue-900'
         } text-white flex flex-col justify-center items-center border-blue-600 lg:hidden gap-12 text-3xl pb-20 font-semibold absolute ${
-          isOpen ? 'top-28' : '-top-full'
+          isOpen ? 'top-20' : '-top-full'
         } bg-white w-full h-screen z-10`}
       >
         <SingleNavLink to="/" linkName="Home" onClick={menuClose} />
@@ -88,22 +89,27 @@ const Navbar = () => {
         />
       </ul>
       <div
-        className={` absolute right-[68px] md:right-80 sm:text-3xl text-5xl ${
-          isDark ? 'text-white' : 'text-zinc-700'
+        className={`hidden md:flex w-[10%] md:w-[5%] justify-center sm:text-3xl text-5xl ${
+          isDark ? 'text-white' : 'text-white'
         }`}
         onClick={() => setIsDark(!isDark)}
       >
-        {isDark ? <MdDarkMode /> : <MdOutlineDarkMode />}
+        {isDark ? <MdDarkMode /> : <FiSun />}
       </div>
 
-      <div>
-        <Search />
+      <Search />
+
+      <div
+        className={`sm:hidden w-[10%] text-3xl ${
+          isDark ? 'text-white' : 'text-white'
+        }`}
+        onClick={() => setIsDark(!isDark)}
+      >
+        {isDark ? <MdDarkMode /> : <FiSun />}
       </div>
 
       <div
-        className={`block lg:hidden  text-6xl mr-2 sm:mr-4 ${
-          isDark ? 'text-white' : 'text-zinc-700'
-        }`}
+        className="block lg:hidden text-5xl text-white"
         onClick={() => setIsOpen(!isOpen)}
       >
         {isOpen ? <RxCrossCircled /> : <BiMenu />}
